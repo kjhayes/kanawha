65d802043446eb8664c1fdb780e6cf5e0a6e3964
Kevin Hayes <KevinHayes2026@u.northwestern.edu>
2025-04-14 19:46:52 -0500

    Split syscall/dir.c into multiple files

---
+++ kernel/syscall/dirattr.c
@@ -0,0 +1,62 @@
+
+#include <kanawha/uapi/dir.h>
+#include <kanawha/syscall.h>
+#include <kanawha/fs/file.h>
+#include <kanawha/kmalloc.h>
+
+#ifdef CONFIG_DEBUG_SYSCALL_DIRATTR
+define LOG(...) printk(__VA_ARGS__)
+#else
+#define LOG(...)
+#endif
+
+int
+syscall_dirattr(
+        struct process *process,
+        fd_t dir_fd,
+        int attr,
+        size_t __user *user_value)
+{
+    int res;
+    struct file *file =
+        file_table_get_file(
+                process->file_table,
+                process,
+                dir_fd);
+    if(file == NULL) {
+        return -EINVAL;
+    }
+
+    size_t value;
+    res = direct_file_dir_readattr(
+            file,
+            attr,
+            &value);
+    if(res) {
+        file_table_put_file(
+                process->file_table,
+                process,
+                file);
+        return res;
+    }
+
+    res = file_table_put_file(
+                process->file_table,
+                process,
+                file);
+    if(res) {
+        return res;
+    }
+
+    res = process_write_usermem(
+            process,
+            user_value,
+            &value,
+            sizeof(size_t));
+    if(res) {
+
+    }
+            
+    return 0;
+}
+
---
+++ kernel/syscall/dirnext.c
@@ -0,0 +1,45 @@
+#include <kanawha/syscall.h>
+#include <kanawha/fs/file.h>
+#include <kanawha/kmalloc.h>
+
+#ifdef CONFIG_DEBUG_SYSCALL_DIRNEXT
+define LOG(...) printk(__VA_ARGS__)
+#else
+#define LOG(...)
+#endif
+
+int
+syscall_dirnext(
+        struct process *process,
+        fd_t dir_fd)
+{
+    int res;
+    struct file *file =
+        file_table_get_file(
+                process->file_table,
+                process,
+                dir_fd);
+    if(file == NULL) {
+        return -EINVAL;
+    }
+
+    res = direct_file_dir_next(file);
+    if(res) {
+        file_table_put_file(
+                process->file_table,
+                process,
+                file);
+        return res;
+    }
+
+    res = file_table_put_file(
+            process->file_table,
+            process,
+            file);
+    if(res) {
+        return res;
+    }
+
+    return 0;
+}
+
