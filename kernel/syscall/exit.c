fe8f26435011b9cb8ae761fffc08377c3f4efc5c
Kevin Hayes <KevinHayes2026@u.northwestern.edu>
2025-06-05 01:10:12 -0500

    slightly better syscall exit panic printout

--- kernel/syscall/exit.c
+++ kernel/syscall/exit.c
@@ -16,8 +16,8 @@ syscall_exit(
 
     res = process_terminate(process, exitcode);
     if(res) {
-        panic("PID(%ld) syscall_exit: process_terminate(%p, %d) -> %s\n",
-              process->id, process, exitcode, errnostr(res));
+        panic("PID(%ld) syscall_exit: process_terminate(%d) -> %s, user_ip=%p\n",
+              process->id, exitcode, errnostr(res), process->user_ip);
     }
 
 #ifdef CONFIG_DEBUG_SYSCALL_EXIT
