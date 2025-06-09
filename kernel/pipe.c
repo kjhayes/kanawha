b3e855277acbfdf48e3142cf3fd01cf6285a3fee
Kevin Hayes <KevinHayes2026@u.northwestern.edu>
2025-06-09 14:50:43 -0500

    pipefs node unload was left uninitialized

--- kernel/pipe.c
+++ kernel/pipe.c
@@ -268,6 +268,7 @@ pipe_fs_mount_load_node(
 
     pipe->fs_node.node_ops = &pipe_fs_node_ops;
     pipe->fs_node.file_ops = &pipe_fs_file_ops;
+    pipe->fs_node.unload = NULL;
 
     res = waitqueue_init(&pipe->read_queue);
     if(res) {
