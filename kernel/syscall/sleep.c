6d81b04269f40ac6cdfd65c5877ea6143ef5d428
Kevin Hayes <KevinHayes2026@u.northwestern.edu>
2025-07-12 12:01:42 -0500

    Remove out-dated sleep.c TODO

--- kernel/syscall/sleep.c
+++ kernel/syscall/sleep.c
@@ -11,11 +11,6 @@ syscall_sleep(
         size_t duration,
         unsigned long flags)
 {
-    // TODO: clk_delay is not a good function to use here at all (it busy waits).
-    //
-    //       Instead we ideally want to set up a timer of some sort
-    //       with a proper callback and set the thread status to SLEEPING
-
     switch(flags) {
         case SLEEP_DURATION_MSEC:
             thread_sleep(msec_to_duration(duration), 0);
